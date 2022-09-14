ad6f16fe9ac488df53981f932836e71f3c76a0dc
tigranargit <73362927+tigranargit@users.noreply.github.com>
2022-09-14 13:00:24 +0100

    finished chapter 09_arrays

---
+++ phase_1/09_arrays_addtobatch.js
@@ -0,0 +1,22 @@
+const addToBatch =(array, number) => {
+      if (array.length < 5) {
+            return newArray = array.concat(number);
+      } else {
+            return array;
+      }  
+}
+
+console.log(addToBatch([1], 3)); 
+//[ 1, 3 ]
+
+console.log(addToBatch([1, 2, 3], 4)); 
+//[ 1, 2, 3, 4 ]
+
+console.log(addToBatch([], 8)); 
+//[ 8 ]
+
+console.log(addToBatch([1, 2, 3, 4, 5, 6], 7)); 
+//[ 1, 2, 3, 4, 5, 6 ]
+
+console.log(addToBatch([1, 2, 3, 4, 5, 6, 7, 8, 9], 10)); 
+//[ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
---
+++ phase_1/09_arrays_exercise.js
@@ -0,0 +1,8 @@
+const names = ['Tig', 'Big', 'Cig']
+const newnames = names.concat(['Dig', 'Fig'])
+
+console.log(names)
+console.log(newnames)
+
+console.log(names.length)
+console.log(newnames[1])
